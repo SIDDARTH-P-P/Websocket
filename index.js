2d1a3fbd44029ca16528b6e7f0e24fb94590e5f8
SIDDARTH_P_P <sidhusidharth2711@gmail.com>
2024-12-30 16:29:03 +0530

    -chore:Port changed

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ wss.on('connection', (ws) => {
 });
 
 
-server.listen(3001, () => {
-    console.log('Server is running at http://localhost:3001');
+server.listen(3000, () => {
+    console.log('Server is running at http://localhost:3000');
 });
