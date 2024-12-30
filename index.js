7bc62574025b56cb06f4dc826d99256974ea650a
SIDDARTH_P_P <sidhusidharth2711@gmail.com>
2024-12-30 16:11:10 +0530

    -feat : added websocket programs

---
+++ index.js
@@ -0,0 +1,32 @@
+const WebSocket = require('ws');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+app.use(express.static(path.join(__dirname)));
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+
+    ws.on('message', (data) => {
+        // Broadcast data to all connected clients except the sender
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+
+server.listen(3001, () => {
+    console.log('Server is running at http://localhost:3001');
+});
